17d74de677b4f3f20d62ea3916008e831e35458d
Rey Diaz <reybdiazjr@gmail.com>
2019-08-15 22:24:41 -0400

    Wrote all the questions in the question format I created

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,5 +1,32 @@
-var questions = [{
-    question: '',
-    choices: [],
-    correctAnswer: '',
-}];
+var questions = [
+    {
+    question: "What phrase is said as the Power Rangers transform?",
+    choices: ["Cowabunga!", "Eat My Shorts!", "Respect My Authority!", "It's Morphin Time"],
+    correctAnswer: "It's Morphin Time",
+},
+
+{
+    question: 'Which Power Ranger is the leader of the group?',
+    choices: ["Red Ranger", "Blue Ranger", "Black Ranger", "Pink Ranger"],
+    correctAnswer: "Red Ranger",
+},
+
+{
+    question: "Which Power Ranger was introduced as 'Evil' but converted to good later in the series?",
+    choices: ["Red Ranger", "Pink Ranger", "Black Ranger", "Green Ranger"],
+    correctAnswer: "Green Ranger",
+},
+
+{
+    question: 'What is the name of the character with the floating head?',
+    choices: ["Zoltar", "Zoltan", "Zordon", "Jeff"],
+    correctAnswer: "Zordon",
+},
+
+{
+    question: 'What color did the weapon did the White Ranger carry?',
+    choices: ["Tiger Sword", "Staff", "Katana", "Power Glove"],
+    correctAnswer: 'Tiger',
+},
+
+];
