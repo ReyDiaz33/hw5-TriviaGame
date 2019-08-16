a5b6e2a0bf0a5b6ac8740bd52d518d0fc8008fe7
Rey Diaz <reybdiazjr@gmail.com>
2019-08-15 20:11:18 -0400

    Pseudo coding my process for JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+/* Instructions
+- You'll create a trivia form with multiple choice or true/false options (your choice).
+- The player will have a limited amount of time to finish the quiz. 
+- The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+- Don't let the player pick more than one answer per question.
+- Don't forget to include a countdown timer.*/
+
+// Psudeo Coding
+// create timer for quiz to appear on top of quiz
+// create variables for your quiz
+// create questions for quiz (Prompts)
+// create answers from variables 
+// create click event for multiple choice answers
+// 
